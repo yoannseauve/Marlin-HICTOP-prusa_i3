8ce59a9291f59d62eef857c73b9003457e37f4cd
yoannseauve <nop@nop.nop>
2024-07-13 13:47:54 +0200

    -switch head to E1 port -z-probe servo soldered to port 12

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -58,7 +58,8 @@
 // Servos
 //
 #ifdef IS_RAMPS_13
-  #define SERVO0_PIN        7 // RAMPS_13 // Will conflict with BTN_EN2 on LCD_I2C_VIKI
+  //#define SERVO0_PIN        7 // RAMPS_13 // Will conflict with BTN_EN2 on LCD_I2C_VIKI
+  #define SERVO0_PIN        12 // Custom board modification
 #else
   #define SERVO0_PIN       11
 #endif
@@ -103,15 +104,27 @@
 #define Z_ENABLE_PIN       62
 #define Z_CS_PIN           40
 
-#define E0_STEP_PIN        26
-#define E0_DIR_PIN         28
-#define E0_ENABLE_PIN      24
-#define E0_CS_PIN          42
+//#define E0_STEP_PIN        26
+//#define E0_DIR_PIN         28
+//#define E0_ENABLE_PIN      24
+//#define E0_CS_PIN          42
+//
+//#define E1_STEP_PIN        36
+//#define E1_DIR_PIN         34
+//#define E1_ENABLE_PIN      30
+//#define E1_CS_PIN          44
 
-#define E1_STEP_PIN        36
-#define E1_DIR_PIN         34
-#define E1_ENABLE_PIN      30
-#define E1_CS_PIN          44
+//swaped because of domage PCB
+#define E0_STEP_PIN        36
+#define E0_DIR_PIN         34
+#define E0_ENABLE_PIN      30
+#define E0_CS_PIN          44
+
+#define E1_STEP_PIN        26
+#define E1_DIR_PIN         28
+#define E1_ENABLE_PIN      24
+#define E1_CS_PIN          42
+
 
 //
 // Temperature Sensors
